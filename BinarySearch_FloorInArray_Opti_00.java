daead36c3aca82f89d6e9273d9ae43eb7816c8b4
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-01-29 07:40:35 +0530

    Create BinarySearch_FloorInArray_Opti_00.java

---
+++ BinarySearch_FloorInArray_Opti_00.java
@@ -0,0 +1,67 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++)
+
+        {
+
+            arr[i] = scn.nextInt();
+
+        }
+
+        int k = scn.nextInt();
+
+        int start = 0, end = n-1; int count=0;
+        int ans = 0;
+
+        while(start<=end)
+
+        {
+
+            int mid = (start+end)/2;
+
+            if(arr[mid]==k)
+
+            {
+
+                ans = arr[mid]; break;
+
+            }
+
+            if(arr[mid]>k)
+
+            {
+
+                end = mid-1;
+
+            }
+
+            if(arr[mid]<k)
+            {
+
+                if(arr[mid]>ans) ans=arr[mid];
+                start = mid+1;
+
+            }
+
+        }
+
+        if(k<arr[0]) System.out.println(-1);
+     
+
+        else System.out.println(ans);
+
+    }
+
+}
