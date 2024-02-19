b31b46dbfb655803a71b9fd14eb7b194b1a1bc9c
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-02-19 15:47:25 +0530

    Update DP_NumberofPaths.java

--- DP_NumberofPaths.java
+++ DP_NumberofPaths.java
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/unique-paths/description/
 class Solution {
-    public int uniquePaths(int m, int n) {             int[][] dp = new int[m][n];
+    public int uniquePaths(int m, int n) {             
+        int[][] dp = new int[m][n];
         for(int i=0;i<m;i++) dp[i][0]=1;
         for(int j=0;j<n;j++) dp[0][j]=1;
         return(func(m-1,n-1,dp));
