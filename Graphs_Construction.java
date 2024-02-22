4937bec72905daf205bee1896906f9396567000d
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-02-22 10:52:54 +0530

    Create Graphs_Construction.java

---
+++ Graphs_Construction.java
@@ -0,0 +1,4 @@
+// Method. n is no. of nodes, m is no. of connections, edges is a 2D array with edge and it's neighbour. one entry for each connection 
+void construction(int n, int m, int[m][2] edges)
+{
+}
