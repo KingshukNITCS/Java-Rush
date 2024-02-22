501b923a3feca299a099a216b16e7aeda4fcba0f
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-02-22 12:25:50 +0530

    Update Graphs_Construction.java

--- Graphs_Construction.java
+++ Graphs_Construction.java
@@ -1,4 +1,12 @@
 // Method. n is no. of nodes, m is no. of connections, edges is a 2D array with edge and it's neighbour. one entry for each connection 
 void construction(int n, int m, int[m][2] edges)
 {
+  List<List<Integer>> graph = new ArrayList<>();
+  for(int i=0; i<m; i++)
+  {
+    int u = edges[i][0];
+    int v = edges[i][1];
+    graph.get(u).add(v);
+    
+  }
 }
