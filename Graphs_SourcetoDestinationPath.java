abdf4010e57411fad2068c7cd10ac7001528f905
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-02-23 16:24:33 +0530

    Update and rename Graphs_StoD.java to Graphs_SourcetoDestinationPath.java

---
+++ Graphs_SourcetoDestinationPath.java
@@ -0,0 +1,32 @@
+/* 
+
+You have been given edges of a graph and a source node(sn) and destination node(dn). Return true if there is a path from source node to destination node or false otherwise.
+Input Format
+
+The first line contains two integers n and m representing no. of nodes and edges
+Next m line represents edges of the graph
+last line contains two integer representing source node(sn) and destination node(dn). 
+Constraints
+
+The nodes are fro 0 to n-1
+Output Format
+
+Return true or False
+Sample Input 0
+
+7 8
+3 4
+1 2
+2 3
+4 6
+0 1
+4 5
+5 6
+0 3
+0 2
+Sample Output 0
+
+true
+
+*/
+
