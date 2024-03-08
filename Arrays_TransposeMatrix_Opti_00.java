a77f1ef41f3142766707eec657f77c63a2aad3be
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-03-08 21:34:02 +0530

    Create Arrays_TransposeMatrix_Opti_00.java

---
+++ Arrays_TransposeMatrix_Opti_00.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[][] arr = new int[n][n];
+        for(int i=0; i<n; i++)
+        {
+        for(int j=0; j<n; j++)
+        {
+            arr[i][j]=scn.nextInt();
+        }         
+        }        
+        for(int i=0; i<n; i++)
+        {
+        for(int j=i+1; j<n; j++)
+        {
+            if(j>i)
+            {
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }         
+        }                
+        for(int w=0; w<n; w++)
+        {
+        for(int z=0; z<n; z++)
+        {
+            System.out.print(arr[w][z]+" ");
+        }
+        System.out.println();
+        }
+    }
+}
