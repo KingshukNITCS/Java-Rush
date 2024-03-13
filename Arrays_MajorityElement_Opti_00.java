516c83ad6666fb6b291156972e473d2378bbd165
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-03-13 17:34:22 +0530

    Create Arrays_MajorityElement_Opti_00.java

---
+++ Arrays_MajorityElement_Opti_00.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int el=0; int freq=0;
+        el=nums[0]; 
+        freq++;
+        for(int i=1; i<n; i++)
+        {
+            if(nums[i]==el) freq++;
+            else
+            {
+                if(freq>0) freq--;
+                else
+                {
+                    el=nums[i]; freq++;
+                }
+            }
+        }
+        return el;
+    }
+}
