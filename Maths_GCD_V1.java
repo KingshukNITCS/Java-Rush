65ae6903f6a85cdebd6351bd5194f34298887aa2
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-05-24 02:43:51 +0530

    Update Maths_GCD_V1.java

--- Maths_GCD_V1.java
+++ Maths_GCD_V1.java
@@ -1,34 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
-        while(t-- > 0){
-            
-            int a;
-            a = Integer.parseInt(br.readLine());
-            
-            
-            int b;
-            b = Integer.parseInt(br.readLine());
-            
-            Solution obj = new Solution();
-            int res = obj.gcd(a, b);
-            
-            System.out.println(res);
-            
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-
 class Solution {
     public static int gcd(int a, int b) {
         if(a>b)
@@ -36,14 +5,7 @@ class Solution {
             int temp=a;
             a=b; b=temp;
         }
-        int modu = b%a;
-        System.out.println(modu);
-        if(modu==0) return a;
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(b%a);
-        gcd(b%a,a);
-        return -1;
+        if(a==0) return b;
+        return gcd(b%a,a);
     }
 }
-
